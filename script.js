f6bd64465c02725c84be1e9457785f9b6f4a817a
heininho <heininho1998@web.de>
2025-10-23 15:43:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 
+// Hauptmenü öffnen/schließen
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
+});
+
+// Plus-Minus-Funktion für Untermenüs
+const toggles = document.querySelectorAll('.menu-toggle');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    const submenu = toggle.nextElementSibling;
+    const symbol = toggle.querySelector('.symbol');
+    
+    submenu.classList.toggle('active');
+    if (submenu.classList.contains('active')) {
+      symbol.textContent = '−'; // Minus
+    } else {
+      symbol.textContent = '+'; // Plus
+    }
+  });
 });
